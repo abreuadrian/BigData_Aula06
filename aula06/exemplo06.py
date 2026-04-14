9e000d086c2cd27ec1613183b4c041b357af958b
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-14 19:50:35 -0300

    add Exs

---
+++ aula06/exemplo06.py
@@ -0,0 +1,27 @@
+#Ex01: Estrutura de repetição (for e while)
+def ex01():
+    start = int(input('Informe o número de inicio: '))      
+    finish = int(input('Informe o número final: '))         
+    inte = int(input('Informe o intervalo: '))
+    for i in range(start, finish+1, inte):                  #range(começo, final, intervalo)
+        print(i, end = " ")                                 #end = " " imprime na mesma linha
+
+#Ex02: 
+def ex02():
+    for i in range (3):
+        print(f'\n{i+1}ª rodada')
+        n1 = int(input('Informe o número: '))
+        n2 = int(input('Informe o segundo número: '))
+        soma = n1 + n2
+        print(f'O total é {soma}')
+
+#Ex03: Variável Acumuladora
+def ex03():
+    x = 0 
+    for i in range(5):
+        num = float(input('Informe o valor de n: '))
+        x += num
+    print(f'O total é {x}')
+ex03()
+
+
