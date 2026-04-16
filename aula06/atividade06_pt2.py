f9fe2763c09d3bda397a66ae9458d6bf9d890d3d
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-16 18:28:04 -0300

    correção atv02

--- aula06/atividade06_pt2.py
+++ aula06/atividade06_pt2.py
@@ -1,6 +1,4 @@
 #Atividade02: 
-import os
-import time
 venda_total = 0
 opt = 's'
 desconto_total =0 
@@ -19,14 +17,13 @@ while opt != 'n':
         desconto_total += desconto
         valor_desconto = venda - (venda * 0.10)
         venda_total += valor_desconto
+        print(f'Venda: R$ {venda}\nDesconto R$: {desconto}\nValor Final: R$: {valor_desconto}')
     else:
+        print(f'Venda: R${venda}')
         venda_total += venda
-    time.sleep(0.5)
-    os.system('cls')
+
     opt = input('Deseja continuar? (s/n): ').lower().strip()
     while opt != 's' and opt != 'n':
         opt = input('Resposta inválida. Digite [s] para sim ou [n] para não: ')
-        time.sleep(0.5)
-        os.system('cls')
 
 print(f'Venda total foi R${venda_total:.2f} e o total de desconto foi R${desconto_total:.2f}')
